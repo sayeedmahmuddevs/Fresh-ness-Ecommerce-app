cda75d5d9ba0bbed488e36a4d544f8619d0ab4a5
Sayeed Mahmud <sayeedmahmud343@gmail.com>
2026-02-23 17:00:04 +0600

    json update

--- fresh-ness/vite.config.js
+++ fresh-ness/vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        about: 'about.html',
+        contact: 'contact.html',
+        allGroceries: 'allGroceries.html',
+        cardDetails: 'cardDetails.html',
+      }
+    }
+  }
 })
-
